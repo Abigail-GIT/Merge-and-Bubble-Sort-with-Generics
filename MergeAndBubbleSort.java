d163b6aca07a9790a4adde10eb9e97b4a9b94fc9
Abigail-GIT <abigailkalaj@oakland.edu>
2025-03-21 22:34:37 -0400

    Add files via upload

---
+++ MergeAndBubbleSort.java
@@ -0,0 +1,79 @@
+import java.util.Random;
+
+public class MergeAndBubbleSort {
+
+    // Bubble Sort implementation
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Merge Sort implementation
+    public static void mergeSort(int[] array) {
+        if (array.length < 2) {
+            return;
+        }
+        int mid = array.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[array.length - mid];
+
+        System.arraycopy(array, 0, left, 0, mid);
+        System.arraycopy(array, mid, right, 0, array.length - mid);
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(array, left, right);
+    }
+
+    private static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = random.ints(10, 0, 101).toArray();
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        // Bubble Sort
+        int[] bubbleSortedArray = array.clone();
+        bubbleSort(bubbleSortedArray);
+        System.out.println("\nBubble Sorted Array:");
+        printArray(bubbleSortedArray);
+
+        // Merge Sort
+        int[] mergeSortedArray = array.clone();
+        mergeSort(mergeSortedArray);
+        System.out.println("\nMerge Sorted Array:");
+        printArray(mergeSortedArray);
+    }
+
+    private static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
